5e39bb369dbc3cbfa8eb523dfb6b1149fee85dc7
Andrii Gargat <andreysax1312@gmail.com>
2022-11-08 15:25:50 +0200

    Handlebars add

--- src/index.js
+++ src/index.js
@@ -1,47 +1,39 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import Handlebars from 'handlebars';
 import API from './fetchCountries';
+import countryListTmp from './templates/country-list.hbs';
+import singleCountryTmp from './templates/single-country.hbs';
 
 const getEl = selector => document.querySelector(selector);
 const inputRef = getEl('#search-box');
 const countryListRef = getEl('.country-list');
 const countryInfoRef = getEl('.country-info');
-
 const DEBOUNCE_DELAY = 300;
 
 inputRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+Handlebars.registerHelper('objValues', Object.values);
 
 function onSearch() {
     const name = inputRef.value.trim();
     API.fetchCountries(name)
-        .then(country => {
-        interfaceRender(country);           
-    })
+        .then(interfaceRender)
         .catch(onFetchError);
 };
 
 function interfaceRender(country) {
     clearMarkup();
-    if (country.length > 10) {
+    const len = country.length;
+
+    if (len > 10) {
             Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-        } else if (country.length <= 10 && country.length > 1) {
-            const markupList = country.map(item => `
-                <li class="country-list__item">
-                    <img class="country__flag" src="${item.flags.svg}" alt="flag">
-                    <p class="country__name">${item.name.official}</p>
-                </li>
-        `).join('');
-        countryListRef.insertAdjacentHTML('beforeend', markupList);
+        } else if (len <= 10 && len > 1) {
+            const markupList = country.map(countryListTmp).join('');
+            countryListRef.insertAdjacentHTML('beforeend', markupList);
         } else {
-            const markupDescription = country.map(item => `
-                <img class="single-country__flag" src="${item.flags.svg}" alt="flag">
-                <p class="single-country__name">${item.name.official}</p>
-                <p class="options">Capital: <span class="value">${item.capital}</span></p>
-                <p class="options">Population: <span class="value">${item.population}</span></p>
-                <p class="options">Languages: <span class="value">${Object.values(item.languages)}</span></p>
-                `).join('');
-            countryInfoRef.insertAdjacentHTML('beforeend', markupDescription);
+            const markupDescription = country.map(singleCountryTmp).join('');
+            countryInfoRef.insertAdjacentHTML('beforeend', markupDescription);  
         }
 };
 
@@ -53,4 +45,3 @@ function clearMarkup() {
     countryListRef.innerHTML = '';
     countryInfoRef.innerHTML = '';
 };
-
